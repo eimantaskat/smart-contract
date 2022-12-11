900b64ca3dcba87f94613f309c3dbc6b258ce1bf
EimantasKat <katinaseimantas@gmail.com>
2022-12-11 14:55:11 +0200

    display orders in web app

--- web-app/pages/deal.tsx
+++ web-app/pages/deal.tsx
@@ -13,12 +13,16 @@ const Deal = () => {
     const [web3, setWeb3] = useState(null);
     const [address, setAddress] = useState(null);
     const [dealContract, setDealContract] = useState(null);
+    const [placedOrders, setPlacedOrders] = useState([]);
+
+    const [seller, setSeller] = useState(false);
+    const [buyer, setBuyer] = useState(false);
+    const [courier, setCourier] = useState(false);
 
     useEffect(() => {
         if (dealContract) {
-            getOrdersHandler();
+            updateView();
         }
-        console.log("address: ", address)
     }, [dealContract, address]);
 
     useEffect(() => {
@@ -32,14 +36,51 @@ const Deal = () => {
     }, [web3]);
 
     useEffect(() => {
-        window.ethereum.on('accountsChanged', (accounts) => {
-            setAddress(accounts[0]);
-        });
+        window.ethereum.on('accountsChanged', handleAccountChange);
     });
+
+    useEffect(() => {
+        getOrders();
+    }, [orders]);
+
+    const handleAccountChange = (accounts) => {
+        setAddress(accounts[0]);
+        updateView();
+        setSeller(false);
+        setBuyer(false);
+        setCourier(false);
+    }
+
+    const updateView = async () => {
+        if (!dealContract) {
+            return;
+        }
+        try {
+            const sellerAddress = await dealContract.methods.sellerAddress().call();
+            if (address === sellerAddress) {
+                getOrdersHandler();
+                getOrders();
+                return setSeller(true);
+            }
+            const buyerAddress = await dealContract.methods.buyerAddress().call();
+            if (address === buyerAddress) {
+                getOrdersHandler();
+                return setBuyer(true);
+            }
+            // TODO courier
+        } catch (err) {
+            if (err instanceof Error) {
+                setError(err.message);
+            } else {
+                setError(`Unexpected error: ${err}`);
+            }
+        }
+    }
 
     const setAccount = async () => {
         const accounts = await web3.eth.getAccounts();
         setAddress(accounts[0]);
+        updateView();
     };
 
     const getOrdersHandler = async () => {
@@ -54,6 +95,24 @@ const Deal = () => {
     const updateAmount = (event : React.ChangeEvent<HTMLInputElement>) => {
         setAmount(event.target.value);
     };
+
+    const getOrders = async () => {
+        try {
+            let contractOrders = [];
+            for (let i = 1; i <= orders; i++) {
+                let order = await dealContract.methods.orders(i).call()
+                contractOrders.push(order);
+            }
+            console.log(contractOrders)
+            setPlacedOrders(contractOrders);
+        } catch (err) {
+            if (err instanceof Error) {
+                setError(err.message);
+            } else {
+                setError(`Unexpected error: ${err}`);
+            }
+        }
+    }
 
     const placeOrderHandler = async () => {
         try {
@@ -103,7 +162,13 @@ const Deal = () => {
                         <h1>Deal</h1>
                     </div>
                     <div className="navbar-end">
-                        <button onClick={connectWalletHandler} type="button" className="btn btn-primary">Connect Wallet</button>
+                        {
+                            address ?
+                                <button onClick={connectWalletHandler} type="button" className="btn btn-primary" disabled >Wallet Connected</button>
+                            :
+                                <button onClick={connectWalletHandler} type="button" className="btn btn-primary">Connect Wallet</button>
+                        }
+                        
                     </div>
                 </div>
             </nav>
@@ -112,31 +177,75 @@ const Deal = () => {
                 <section>
                     <section>
                         <div className="container text-primary">
-                            <h4>Connected wallet: {address}</h4>
+                            <h4>Connected wallet: {address}
+                            { seller && <section>(Seller)</section> }
+                            { buyer && <section>(Buyer)</section> }
+                            { courier && <section>(Courier)</section> }
+                            </h4>
                         </div>
                     </section>
-                    <section>
-                        <div className="container">
-                            <h2>Orders: {orders}</h2>
-                        </div>
-                    </section>
-                    <section>
-                        <div className="container">
-                            <label htmlFor="order" className="label">Place order</label>
-                            <div className="input-group mb-3" id="order">
-                                <input onChange={updateItem} type="text" className="form-control" placeholder="Item" />
-                            </div>
-                            <div className="input-group mb-3" id="order">
-                                <input onChange={updateAmount} type="number" className="form-control" placeholder="Amount" />
-                            </div>
-                            <button onClick={placeOrderHandler} type="button" className="btn btn-primary">Place order</button>
-                        </div>
-                    </section>
-                    <section>
-                        <div className="container text-danger">
-                            <p>{error}</p>
-                        </div>
-                    </section>
+                    {
+                        buyer && 
+                        <section>
+                            <section>
+                                <div className="container">
+                                    <h2>Orders: {orders}</h2>
+                                </div>
+                            </section>
+                            <section>
+                                <div className="container">
+                                    <label htmlFor="order" className="label">Place order</label>
+                                    <div className="input-group mb-3" id="order">
+                                        <input onChange={updateItem} type="text" className="form-control" placeholder="Item" />
+                                    </div>
+                                    <div className="input-group mb-3" id="order">
+                                        <input onChange={updateAmount} type="number" className="form-control" placeholder="Amount" />
+                                    </div>
+                                    <button onClick={placeOrderHandler} type="button" className="btn btn-primary">Place order</button>
+                                </div>
+                            </section>
+                            <section>
+                                <div className="container text-danger">
+                                    <p>{error}</p>
+                                </div>
+                            </section>
+                        </section>
+                    }
+                    {
+                        placedOrders.length > 0 &&
+                        <section>
+                            <table className="table">
+                                <thead>
+                                    <tr>
+                                    <th scope="col">#</th>
+                                    <th scope="col">Product</th>
+                                    <th scope="col">Quantity</th>
+                                    <th scope="col">Price</th>
+                                    <th scope="col">Payment</th>
+                                    <th scope="col">Stage</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <>
+                                    {
+                                        placedOrders.map((order, index) => {
+                                            return (
+                                                <tr key={index}>
+                                                    <td>{index}</td>
+                                                    <td>{order.product}</td>
+                                                    <td>{order.quantity}</td>
+                                                    <td>{order.price}</td>
+                                                    <td>{order.payment}</td>
+                                                    <td>{order.stage}</td>
+                                                </tr>
+                                            )
+                                        })
+                                    }
+                                    </>
+                                </tbody>
+                            </table>
+                        </section>
+                    }
                 </section>
                 :
                 <div className="container text-info">
